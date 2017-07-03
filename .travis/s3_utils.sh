514efe9d7e8d28fe94e9305f7835198a47fbd4e7
andy.rothwell <rothwell.andy@gmail.com>
2017-07-03 14:19:39 -0400

    fixed travis bug

--- .travis/s3_utils.sh
+++ .travis/s3_utils.sh
@@ -2,11 +2,7 @@
 
 # this returns the s3 bucket name for a git branch
 get_s3_bucket() {
-<<<<<<< HEAD
-  local S3_BUCKET="betsy"
-=======
   local S3_BUCKET="betsy.phila.gov"
->>>>>>> develop
   # if this isn't a tag build and the branch is something other than master...
   # (tag builds always get pushed to master, and non-master branches get pushed
   # to their own bucket)
