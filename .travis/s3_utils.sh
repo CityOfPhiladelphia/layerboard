16f27a22174250756872ba9bfbfc8cbb648f4952
andy.rothwell <rothwell.andy@gmail.com>
2017-07-03 10:22:32 -0400

    committing to develop

--- .travis/s3_utils.sh
+++ .travis/s3_utils.sh
@@ -2,7 +2,7 @@
 
 # this returns the s3 bucket name for a git branch
 get_s3_bucket() {
-  local S3_BUCKET="mapboard"
+  local S3_BUCKET="betsy"
   # if this isn't a tag build and the branch is something other than master...
   # (tag builds always get pushed to master, and non-master branches get pushed
   # to their own bucket)
