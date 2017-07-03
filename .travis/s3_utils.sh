1364c324cce1fbf7a523396433a27b39341e96ed
andy.rothwell <rothwell.andy@gmail.com>
2017-07-03 14:15:51 -0400

    setting up master and dev in travis

--- .travis/s3_utils.sh
+++ .travis/s3_utils.sh
@@ -2,13 +2,14 @@
 
 # this returns the s3 bucket name for a git branch
 get_s3_bucket() {
-  local S3_BUCKET="betsy-dev.phila.gov"
+  local S3_BUCKET="betsy.phila.gov"
   # if this isn't a tag build and the branch is something other than master...
   # (tag builds always get pushed to master, and non-master branches get pushed
   # to their own bucket)
-  # if [ -z "$TRAVIS_TAG" ] && [ $TRAVIS_BRANCH != "master" ]; then
-  #   S3_BUCKET+="-$TRAVIS_BRANCH"
-  # fi
+  if [ -z "$TRAVIS_TAG" ] && [ $TRAVIS_BRANCH != "master" ]; then
+    # S3_BUCKET+="-$TRAVIS_BRANCH"
+    S3_BUCKET="betsy-dev.phila.gov"
+  fi
   echo $S3_BUCKET
   return 0
 }
