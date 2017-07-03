b8a75c55d1b37bf4dd7eab1e21d8487a6cf1a128
andy.rothwell <rothwell.andy@gmail.com>
2017-07-03 14:08:24 -0400

    still testing travis

--- .travis/s3_utils.sh
+++ .travis/s3_utils.sh
@@ -2,13 +2,13 @@
 
 # this returns the s3 bucket name for a git branch
 get_s3_bucket() {
-  local S3_BUCKET="betsy"
+  local S3_BUCKET="betsy-dev.phila.gov"
   # if this isn't a tag build and the branch is something other than master...
   # (tag builds always get pushed to master, and non-master branches get pushed
   # to their own bucket)
-  if [ -z "$TRAVIS_TAG" ] && [ $TRAVIS_BRANCH != "master" ]; then
-    S3_BUCKET+="-$TRAVIS_BRANCH"
-  fi
+  # if [ -z "$TRAVIS_TAG" ] && [ $TRAVIS_BRANCH != "master" ]; then
+  #   S3_BUCKET+="-$TRAVIS_BRANCH"
+  # fi
   echo $S3_BUCKET
   return 0
 }
