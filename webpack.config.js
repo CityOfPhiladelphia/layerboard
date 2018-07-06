9128216f884a7a0510c94a810a75dceb1490d4f8
andy.rothwell <rothwell.andy@gmail.com>
2018-07-06 14:01:31 -0400

    uses later releases of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,51 @@
 var path = require('path')
 var webpack = require('webpack')
+var VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+var env = process.env.NODE_ENV;
+var isDevelopment = env === 'development';
 
 module.exports = {
-  entry: './src/main.js',
+  entry: {
+    app: './src/main.js',
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+    // alias: {
+    //   'vue$': 'vue/public/vue.esm.js'
+    // }
+  },
+  devtool: isDevelopment ? 'inline-source-map' : 'source-map',
+  devServer: {
+    contentBase: './public',
+    historyApiFallback: true,
+    // noInfo: true,
+    host: process.env.WEBPACK_DEV_HOST,
+    port: process.env.WEBPACK_DEV_PORT
+  },
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    path: path.resolve(__dirname, 'public'),
     filename: 'build.js',
-    library: 'Mapboard'
+    publicPath: '/',
+    library: 'WebMapViewer',
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
         loader: 'vue-loader',
-        options: {
-          loaders: {
-          }
-          // other vue-loader options go here
-        }
       },
       {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
@@ -34,56 +56,49 @@ module.exports = {
       }
     ]
   },
-  resolve: {
-    extensions: ['.js', '.vue'],
-    // alias: {
-    //   'vue$': 'vue/dist/vue.esm.js'
-    // }
-  },
   externals: {
     'vue': 'Vue',
     'leaflet': 'L',
     'jQuery': '$',
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-    host: 'localhost',
-    // host: process.env.WEBPACK_DEV_HOST,
-    port: process.env.WEBPACK_DEV_PORT
   },
   performance: {
     hints: false
   },
   devtool: '#eval-source-map',
   plugins: [
-    new webpack.DefinePlugin({
-      // this allows webpack to interpolate the value of the WEBPACK_DEV_HOST
-      // env var during build.
-      'process.env.WEBPACK_DEV_HOST': JSON.stringify(
-                                        process.env.WEBPACK_DEV_HOST
-                                      )
-    })
-  ]
-}
+    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
+    new VueLoaderPlugin(),
+    // new webpack.DefinePlugin({
+    //   // this allows webpack to interpolate the value of the WEBPACK_DEV_HOST
+    //   // env var during build.
+    //   'process.env.WEBPACK_DEV_HOST': JSON.stringify(
+    //                                     process.env.WEBPACK_DEV_HOST
+    //                                   )
+    // })
+  ],
+  mode: env,
+  // optimization: {
+  //
+  // },
+};
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
-}
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports.devtool = '#source-map'
+//   // http://vue-loader.vuejs.org/en/workflow/production.html
+//   module.exports.plugins = (module.exports.plugins || []).concat([
+//     new webpack.DefinePlugin({
+//       'process.env': {
+//         NODE_ENV: '"production"'
+//       }
+//     }),
+//     new webpack.optimize.UglifyJsPlugin({
+//       sourceMap: true,
+//       compress: {
+//         warnings: false
+//       }
+//     }),
+//     new webpack.LoaderOptionsPlugin({
+//       minimize: true
+//     })
+//   ])
+// }
